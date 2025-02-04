8e055b618c4b2a653d324d7dd32275133c32f1f8
Keny Travanino <keny03@unitec.edu>
2025-02-03 22:08:26 -0600

    initial commit

---
+++ lib/src/features/login/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_frontend/src/features/login/login_view.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginView();
+  }
+}
