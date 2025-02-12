9bd57798fc7b388fbae30f503d7a29404e736959
Keny Travanino <keny03@unitec.edu>
2025-02-12 06:07:47 -0600

    Added: new model user

--- lib/src/domain/domain.dart
+++ lib/src/domain/domain.dart
@@ -1,4 +1,7 @@
-export './datasources/irole_datasource.dart';
+export 'abstractions/datasources/irole_datasource.dart';
+export 'abstractions/datasources/iauth_datasource.dart';
 export './models/role.dart';
-export './repositories/irole_repository.dart';
+export './models/user.dart';
+export 'abstractions/repositories/irole_repository.dart';
+export 'abstractions/repositories/iauth_repository.dart';
 export './routes/app_routes.dart';
---
+++ lib/src/domain/models/user.dart
@@ -0,0 +1,69 @@
+import 'package:mobile_frontend/src/domain/domain.dart';
+
+class User {
+  final String id;
+  final String names;
+  final String lastNames;
+  final String email;
+  final String password;
+  final String phone;
+  final String description;
+  final Role role;
+
+  User({
+    this.id = '',
+    required this.names, 
+    required this.lastNames, 
+    required this.email, 
+    required this.password, 
+    required this.phone, 
+    this.description = '',
+    Role? role
+  }) : role = role ?? Role(description: '') ;
+
+  factory User.fromMap(Map<String, dynamic> json) => User(
+    id: json["_id"],
+    names: json['names'],
+    lastNames: json['lastNames'],
+    email: json['email'],
+    password: json['password'] ?? '',
+    phone: json['phone'],
+    description: json["description"],
+    role: Role.fromMap(json['roleId'])
+  );
+
+  Map<String, dynamic> toJson() => {
+    // "_id": id,
+    "names": names,
+    "lastNames": lastNames,
+    "email": email,
+    "password": password,
+    "phone": phone,
+    "description": description,
+    'roleId': role.id,
+    // "role": role?.toJson(),  // Convierte role a JSON solo si no es null
+  };
+
+  User copyWith({
+    String? id
+    ,String? names
+    ,String? lastNames
+    ,String? email
+    ,String? password
+    ,String? phone
+    ,String? description
+    ,Role? role
+  }){
+    return User(
+      id: id ?? this.id,
+      names: names ?? this.names, 
+      lastNames: lastNames ?? this.lastNames, 
+      email: email ?? this.email, 
+      password: password ?? this.password, 
+      phone: phone ?? this.phone, 
+      description: description ?? this.description,
+      role: role ?? this.role
+    );
+  }
+  
+}
