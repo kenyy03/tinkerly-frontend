376502cbdf26c1878abfa6574a2dae3120ee99c0
Keny Travanino <keny03@unitec.edu>
2025-02-21 02:51:12 -0600

    Remove: debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       routerConfig: router,
       title: 'Tinkerly',
       theme: AppTheme.defaultTheme,
