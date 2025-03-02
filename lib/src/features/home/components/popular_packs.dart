b6608b3fe3b258c645943a589e1b37ea18f99254
Keny Travanino <keny03@unitec.edu>
2025-03-02 00:21:01 -0600

    added: popular profiles

---
+++ lib/src/features/home/components/popular_packs.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mobile_frontend/src/domain/domain.dart';
+import 'package:mobile_frontend/src/features/common/components/bundle_tile_square.dart';
+import 'package:mobile_frontend/src/features/common/components/title_and_action_button.dart';
+import 'package:mobile_frontend/src/utils/constants/constants.dart';
+
+
+class PopularPacks extends StatelessWidget {
+  const PopularPacks({
+    super.key,
+    required this.usersData
+  });
+
+  final List<User> usersData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TitleAndActionButton(
+          title: 'Perfiles Populares',
+          onTap: () => context.push(AppRoutes.popularItems),
+        ),
+        SingleChildScrollView(
+          padding: const EdgeInsets.only(left: AppDefaults.padding),
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(
+              usersData.length,
+              (index) => Padding(
+                padding: const EdgeInsets.only(right: AppDefaults.padding),
+                child: BundleTileSquare(data: usersData[index]),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: AppDefaults.margin)
+      ],
+    );
+  }
+}
